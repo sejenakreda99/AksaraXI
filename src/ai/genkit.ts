c2cfdc52fd3faf7cfbc654229fb1771efc6c787f
Kuswara Senjaya <sejenakreda99@gmail.com>
2025-08-11 02:02:51 +0000

    saya menambahkan kelompok baru beserta email dan paswordnya namun setela

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,14 +1,15 @@
 import { genkit } from 'genkit';
 import { googleAI } from '@genkit-ai/googleai';
-import { firebase } from '@genkit-ai/firebase';
+import { firebase } from '@genkit-ai/firebase/plugin';
 import { nextjs } from '@genkit-ai/next';
-
+ 
 export const ai = genkit({
   plugins: [
     nextjs(),
     firebase(),
     googleAI(),
   ],
+ 
   logLevel: 'debug',
   enableTracingAndMetrics: true,
 });
